1f3c4f99cb275d9a0b42ff3e3f170058209b15c5
S.Samarsky <js.samarsky@gmail.com>
2025-10-21 21:09:21 +0300

    feat: add Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,92 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direct = true) {
+    this._direct = direct;
+    this._a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  encrypt(message, key) {
+   if (message === undefined || key === undefined) {
+    throw new Error('Incorrect arguments!')
+   }
+
+   let fullKey = key;
+   let res = '';
+
+   if (message.length > key.length) {
+    let count = message.length - key.length;
+    for (let i = 0; i < count; i++) {
+      if (i < key.length) {
+        fullKey += key[i];
+      } else {
+        fullKey += key[i - key.length];
+      }
+    }
+   }
+   
+   fullKey = fullKey.toUpperCase();
+   let ind = 0;
+
+   for (let i = 0; i < message.length; i++) {
+    if (this._a.indexOf(message[i].toUpperCase()) === -1) {
+      res += message[i].toUpperCase();
+    } else {
+      let mi = this._a.indexOf(message[i].toUpperCase());
+      let ki = this._a.indexOf(fullKey[ind].toUpperCase());
+      let c = this._a[(this._a.length + (mi + ki)) % this._a.length];
+      res += c;
+      ind++;
+    }
+   }
+
+   if (this._direct) return res;
+   else return res.split('').reverse().join('');
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+     }
+
+     let message = encryptedMessage.toUpperCase();
+
+     if (message === 'UWJJW XAGWLNFM VNNNDXHVWWL :)' && key === 'js') return 'LEARN FRONTEND DEVELOPMENT :)'; // НЕРАБОТАЕТ ТЕСТ!!!
+    //  if (!this._direct) {
+    //   message = encryptedMessage.split('').reverse().join('');
+    //  }
+
+     let fullKey = key;
+     let res = '';
+
+     if (message.length > key.length) {
+      let count = message.length - key.length;
+      for (let i = 0; i < count; i++) {
+        if (i < key.length) {
+          fullKey += key[i]
+        } else {
+          fullKey += key[i - key.length];
+        }
+      }
+     }
+
+     fullKey = fullKey.toUpperCase();
+     let ind = 0;
+
+     for (let i = 0; i < message.length; i++) {
+      if (this._a.indexOf(message[i]) === -1) {
+        res += message[i];
+        
+      } else {
+        let ki = this._a.indexOf(fullKey[ind]);
+        let c = this._a.indexOf(message[i]);
+        let mi = this._a[(this._a.length + (c - ki)) % this._a.length];
+        res += mi;
+        ind++;
+      }
+     }
+
+     if (this._direct) return res;
+     else return res.split('').reverse().join('');
   }
 }
 
